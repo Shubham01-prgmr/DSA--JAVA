bef1e20680e61f610c25461daad4a63792f4cd5d
Shubham01-prgmr <sks9508792911@gmail.com>
2025-12-19 14:15:48 +0530

    Added kadane solution with all test cases, including array with no elements and array with -ve elements O(n)

--- Arrays/kadane.java
+++ Arrays/kadane.java
@@ -1,18 +1,18 @@
 public class kadane {
-    public static void main(String[] args) {
-        int nums[] = {-2,-3,4,-1,-2,1,5,-3};
-        System.out.println("Maximum subarray sum is: " + kadaneMaxSubarray(nums));
-    }
-    public static int kadaneMaxSubarray(int nums[]){
-        int max_sum = Integer.MIN_VALUE;
-        int curr_sum = 0;
-        for(int i = 0; i < nums.length; i++){
-            curr_sum += nums[i];
-            if(curr_sum < 0){
-                curr_sum = 0;
-            }
-            max_sum = Math.max(max_sum, curr_sum);
+    public int maxSubArray(int[] nums) {
+
+        if (nums == null || nums.length == 0) {
+            throw new IllegalArgumentException("Array must not be empty");
         }
-        return max_sum;
+
+        int currSum = nums[0];
+        int maxSum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            currSum = Math.max(nums[i], currSum + nums[i]);
+            maxSum = Math.max(maxSum, currSum);
+        }
+
+        return maxSum;
     }
 }
