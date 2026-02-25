66829cf3cd716ff8b58c3c75f3eae543c6a40fd0
Shubham01-prgmr <sks9508792911@gmail.com>
2026-02-26 00:00:10 +0530

    added level order sp;ution

--- Trees/LevelOrderTree.java
+++ Trees/LevelOrderTree.java
@@ -1,5 +1,5 @@
-
-public class BinaryTree {
+import java.util.*;
+public class LevelOrderTree {
     static class Node {
         int data;
         Node left;
@@ -25,13 +25,34 @@ public class BinaryTree {
             
             return newNode;
         }
-        public static void preOrder(Node root){
+        public static void levelOrder(Node root){
             if(root == null){
                 return;
             }
-            System.out.print(root.data + " ");
-            preOrder(root.left);
-            preOrder(root.right);
+            Queue<Node> q = new LinkedList<>();
+            q.add(root);
+            q.add(null);
+             while(!q.isEmpty()){
+                Node curr = q.remove();
+                if(curr == null){
+                    System.out.println();
+                    if(q.isEmpty()){
+                        break;
+                    }
+                    else{
+                        q.add(null);
+                    }
+                }
+                else{
+                    System.out.print(curr.data + " ");
+                    if(curr.left != null){
+                        q.add(curr.left);
+                    }
+                    if(curr.right != null){
+                        q.add(curr.right);
+                    }
+                }
+            }
         }
     }
 
@@ -39,6 +60,6 @@ public class BinaryTree {
         int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
         BinaryTreeB tree = new BinaryTreeB();
         Node root = tree.buildTree(nodes);
-        tree.preOrder(root);
+        tree.levelOrder(root);
     }
 }
