f9ae5b5b995f1d8b02a0a1cc31c12d59420a0d5a
Shubham01-prgmr <sks9508792911@gmail.com>
2026-07-19 23:59:31 +0530

    Added solution for course schedule 2

--- CourseSchedule2.java
+++ CourseSchedule2.java
@@ -1,59 +1,68 @@
 import java.util.*;
 
-public class CouseSchedule {
+public class CourseSchedule2{
     public static void main(String[] args) {
-
         int numCourses = 4;
-
         int[][] prerequisites = {
                 {1, 0},
-                {0, 1},
+                {2, 0},
                 {3, 1},
                 {3, 2}
         };
         ArrayList<Integer> graph[] = new ArrayList[numCourses];
-
         for(int i = 0; i < graph.length; i++){
             graph[i] = new ArrayList<>();
         }
         for(int p[] : prerequisites){
             int course = p[0];
             int prereq = p[1];
-
             graph[prereq].add(course);
-        }      
-        System.out.println(courseSchedule(numCourses, graph));
+        }
+        System.out.println(courseScheduleII(graph, prerequisites, numCourses));
     }
-    public static boolean courseSchedule(int numCourses, ArrayList<Integer> graph[]){
+    public static int[] courseScheduleII(ArrayList<Integer> graph[], int prerequisites[][],
+        int numCourses){
+        ArrayList<Integer> order = new ArrayList<>();
         Queue<Integer> q = new LinkedList<>();
-        int indegree[] = new int[graph.length];
-        countIndegree(graph, indegree);
+        int indegree[] = new int[numCourses];
+        int count = 0;
+
+        findIndegree(indegree, graph);
 
         for(int i = 0; i < indegree.length; i++){
             if(indegree[i] == 0){
                 q.offer(i);
             }
         }
-        int count = 0;
         while(!q.isEmpty()){
-            int curr = q.remove();
+            int curr = q.poll();
             count++;
-            
+            order.add(curr);
             for(int neighbor : graph[curr]){
                 indegree[neighbor]--;
+
                 if(indegree[neighbor] == 0){
                     q.offer(neighbor);
                 }
             }
         }
-        return count == numCourses;
+        if (count != numCourses) {
+            return new int[0];
+        }
+
+        int[] ans = new int[order.size()];
+
+        for (int i = 0; i < order.size(); i++) {
+            ans[i] = order.get(i);
+        }
+
+        return ans;
     }
-    public static void countIndegree(ArrayList<Integer> graph[], int indegree[]){
-        for(int i = 0; i < indegree.length; i++){
+    public static void findIndegree(int indegree[], ArrayList<Integer> graph[]){
+        for(int i = 0; i < graph.length; i++){
             for(int neighbour : graph[i]){
                 indegree[neighbour]++;
             }
         }
     }
 }
-
